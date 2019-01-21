14d06b5c4f2950089e500fe8db27a3ac2274504b
Mark Mauro <markymauro@gmail.com>
2019-01-21 11:27:24 -0500

    Update circle.java

--- circle.java
+++ circle.java
@@ -28,9 +28,10 @@ public class circle extends JFrame{
 		    g.fillOval(250, 150, 350, 350);
 		    
 		    
-		    g.setColor(Color.LIGHT_GRAY);
-		    
+		    g.setColor(getBackground());
 		    g.fillArc(275, 175, 300, 300, 0, 360);
+		    
+	
 		    
 		    Graphics2D g2 = (Graphics2D) g;
 		    g.setColor(Color.black);
@@ -38,24 +39,46 @@ public class circle extends JFrame{
 		    
 		    // THIS IS THE X AND Y AXIS
 		    
-		    g.drawLine(430, 152 , 430, 498); 	// vertical line
-		    g.drawLine(253, 340, 597, 340);		// horizontal line
+		    g.drawLine(430, 152 , 430, 498); 	// vertical line, 0 and 180 degrees
+		    g.drawLine(253, 340, 597, 340);		// horizontal line , 90 and 270 degrees
 		 
-		    Font font = new Font("Arial", Font.BOLD, 25);
+		    Font font = new Font("Arial", Font.BOLD, 20);
 		    g.setFont(font);
+		    g.setColor(Color.DARK_GRAY);
+		    
+		    g.drawString("0°----- 0 ----- (1,0)", 500, 337);	// the x
+		    g.drawString("(-1,0) --- \u03c0 --- 180°", 200, 337); //the x
+		    
+		    Font font1 = new Font("Arial", Font.BOLD, 17);
+		    g.setFont(font1);
+		    g.drawString("30° --- \u03c0/6 --- (\u221A3/2, 1/2)", 590, 250); // 30 degrees
+		    g.drawString("45° --- \u03c0/4 --- (\u221A2/2, 2/2) ", 550, 200); // 45 degrees
+		    g.drawString("60° --- \u03c0/3 --- (1/2, \u221A3/2)", 490, 160);
+		    
+		    g.setFont(font);
+		    g.drawString("(0,1)", 415, 70);
+		    g.drawString("-", 430, 85);
+		    g.drawString("\u03c0/2", 420, 100);
+		    g.drawString("-", 430, 114);
+		    g.drawString("90°", 420, 130);
+		    
+		    
 		    g.setColor(Color.black);
-		    
-		    g.drawString("0°, 0, (1,0)", 610, 350);	
-		    g.drawString("(-1,0) , \u03c0 , 180°", 55, 350);
-		    
-		    // THESE ARE THE 45 DEGREE ANGLES
-		    
 		    g2.setStroke(new BasicStroke(3)); 
-		    g.drawLine(320, 465, 550, 205);		// 45 and 225 degrees	// these numbers are similar but not the same for some reason
+		    g.drawLine(320, 465, 550, 205);		// 45 and 225 degrees	 
 		    g.drawLine(290, 215, 550, 447);		// 135 and 315 degrees
+		    g.drawLine(370, 490 , 500, 170);    // 60 and 240 degrees
 		    
+		    g.drawLine(282, 432, 580, 250);		// 30 and 210 degrees
+		    g.drawLine(350, 170, 500, 483);		// 120 and 300 degrees
+		    g.drawLine(260, 275, 583, 398); 		// 150 and 330 degrees
 		    
-		  
+		
+		    g.setColor(Color.black);
+		    g.fillOval(405, 313, 50, 50);
+		    
+		    g.setColor(Color.YELLOW);	 
+		    g.fillArc(410, 318, 40, 40, 0, 360);
 	}
 	
 }
